c2c58bc840a05bed7f025096e7b46fd82a075522
Luca Bianchi <bianchi.luca@gmail.com>
2022-04-30 22:03:17 +0200

    :sparkles: Initial site creation

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,29 +2,64 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `coherency.com`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Coherency`,
+    siteUrl: `https://www.coherency.eu`,
+    description: `Technology becomes relevant when connects people in a coherent, holistic experience.`,
+    author: `@coherency`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+  {
+    resolve: '@chakra-ui/gatsby-plugin',
+    options: {
+      /**
+       * @property {boolean} [resetCSS=true]
+       * if false, this plugin will not use `<CSSReset />
+       */
+      resetCSS: true,
+      /**
+       * @property {boolean} [isUsingColorMode=true]
+       * if false, this plugin will not use <ColorModeProvider />
+       */
+      isUsingColorMode: true,
+    },
+  },
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
+    },
+    {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "blog",
+      "path": "./blog/"
+    },
+    __key: "blog"
+    },
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+    }
+  ]
 };
 
 export default config;
---
+++ src/layout/PageLayout.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const PageLayout:FC = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
+
+export default PageLayout;
